29b2dc947329f32e76cebdd35cfdd00659f5fffc
Nicholas Li <58043224+Livingwell088@users.noreply.github.com>
2025-02-20 14:23:01 -0500

    User refactor done

--- src/components/LoginModal.js
+++ src/components/LoginModal.js
@@ -22,6 +22,9 @@ const LoginModal = (props) => {
     const [passReq, setPassReq] = useState(false)
     const [validated, setValidated] = useState(false);
 
+    const [passwordMatchError, setPasswordMatchError] = useState("");
+    const [passwordError, setPasswordError] = useState("");
+
     const [showError, setShowError] = useState(false)
     const [errorHeading, setErrorHeading] = useState("")
     const [errorContent, setErrorContent] = useState("")
@@ -46,6 +49,19 @@ const LoginModal = (props) => {
         const value = event.target.value;
 
         setInputs(values => ({...values, [name]: value}))
+
+        if (!(inputs.password.length >= 8)){
+            setPasswordError("Password must be at least 8 characters long")
+        }
+        else if (!(/\d/.test(inputs.password))){
+            setPasswordError("Password must contain a number")
+        }
+        else if (!(/[a-z]/.test(inputs.password))){
+            setPasswordError("Password must contain a lowercase letter")
+        }
+        else if (!(/[A-Z]/.test(inputs.password))){
+            setPasswordError("Password must contain a uppercase letter")
+        }
 
     }
 
@@ -122,36 +138,87 @@ const LoginModal = (props) => {
         }
     }
 
-    const handleSignUp = (event) => {
+    // const handleSignUp = (event) => {
+    //
+    //     const form = event.currentTarget;
+    //     if (form.checkValidity() === false || inputs.password !== inputs.passwordConfirm) {
+    //         event.preventDefault();
+    //         setValidated(true)
+    //         event.stopPropagation();
+    //     }
+    //     else{
+    //         setValidated(true)
+    //
+    //
+    //
+    //         if (inputs.password !== inputs.passwordConfirm){
+    //             alert("Passwords Must Match!")
+    //             clearStates()
+    //         }
+    //
+    //         else{
+    //             API.userAPI.create(inputs.username, inputs.password, inputs.firstName, inputs.lastName, inputs.email, false)
+    //                 .then(r => console.log(r))
+    //                 .then(() => handleRefresh)
+    //                 .catch((error) => console.log(error.message))
+    //         }
+    //
+    //     }
+    //
+    // }
 
+    const handleSignUp = (event) => {
+        event.preventDefault()
         const form = event.currentTarget;
-        if (form.checkValidity() === false || inputs.password !== inputs.passwordConfirm) {
-            event.preventDefault();
+        if (form.checkValidity() === false || inputs.password !== inputs.passwordConfirm || !passwordIsValid) {
+            event.stopPropagation()
             setValidated(true)
-            event.stopPropagation();
+
+
+            if (!passwordIsValid) {
+                if (!(inputs.password.length >= 8)){
+                    setPasswordError("Password must be at least 8 characters long")
+                    console.log("Password 8 characters")
+                }
+                else if (!(/\d/.test(inputs.password))){
+                    setPasswordError("Password must contain a number")
+                    console.log("Password must contain a number")
+                }
+                else if (!(/[a-z]/.test(inputs.password))){
+                    setPasswordError("Password must contain a lowercase letter")
+                    console.log("Password must contain a lowercase letter")
+                }
+                else if (!(/[A-Z]/.test(inputs.password))){
+                    setPasswordError("Password must contain a uppercase letter")
+                    console.log("Password must contain a uppercase letter")
+                }
+            }
+            else if (inputs.password !== inputs.passwordConfirm) {
+                setPasswordMatchError("Passwords must match")
+                setPasswordError("")
+                console.log("Password doesn't match")
+
+            }
+            else{
+                setPasswordMatchError("")
+                setPasswordError("")
+            }
         }
         else{
             setValidated(true)
+            setPasswordMatchError("")
+            setPasswordError("")
 
+            console.log("Testing ")
 
-
-            if (inputs.password !== inputs.passwordConfirm){
-                alert("Passwords Must Match!")
-                clearStates()
-            }
-
-            else{
-                API.userAPI.create(inputs.username, inputs.password, inputs.firstName, inputs.lastName, inputs.email, false)
-                    .then(r => console.log(r))
-                    .then(() => handleRefresh)
-                    .catch((error) => console.log(error.message))
-            }
-
+            API.userAPI.create(inputs.username, inputs.password, inputs.firstName, inputs.lastName, inputs.email, false)
+                .then(r => console.log(r))
+                .then(() => handleRefresh)
+                .catch((error) => console.log(error.message))
         }
-
     }
 
-    const passwordIsValid = inputs.password.length >= 8;
+    const passwordIsValid = inputs.password.length >= 8 && /\d/.test(inputs.password) && /[a-z]/.test(inputs.password) && /[A-Z]/.test(inputs.password);
 
     const validSignUp = () => {
 
@@ -244,7 +311,7 @@ const LoginModal = (props) => {
 
 
 
-                {!loginScreen && <Form validated={validated}
+                {!loginScreen && <Form noValidate validated={validated} onSubmit={handleSignUp}
                 >
 
                     <Row>
@@ -367,13 +434,13 @@ const LoginModal = (props) => {
                                     onChange={handleChange}
                                     placeholder={"Password"}
                                     style={{width: "100%"}}
-                                    isInvalid={(inputs.password === "0")}
+                                    isInvalid={!passwordIsValid}
                                     // isValid={false}
                                     required
                                 />
 
                                 <Form.Control.Feedback type="invalid">
-                                    Password Required
+                                    {passwordError || "Password Required"}
                                 </Form.Control.Feedback>
                             </FloatingLabel>
                         </Col>
@@ -391,10 +458,11 @@ const LoginModal = (props) => {
                                     onChange={handleChange}
                                     placeholder={"Confirm Password"}
                                     style={{width: "100%"}}
+                                    isInvalid={inputs.passwordConfirm !== inputs.password}
                                     required
                                 />
                                 <Form.Control.Feedback type="invalid">
-                                    Confirm Password Required
+                                    {passwordMatchError || "Passwords must match"}
                                 </Form.Control.Feedback>
                             </FloatingLabel>
 
@@ -405,7 +473,8 @@ const LoginModal = (props) => {
 
                     <br />
 
-                    <Button onClick={handleSignUp} disabled={!(inputs.username !== "" && inputs.password !== "" && inputs.firstName !== "" && inputs.lastName !== "" && inputs.email !== "" && inputs.passwordConfirm !== "")}>Sign Up</Button>
+                    {/*disabled={!(inputs.username !== "" && inputs.password !== "" && inputs.firstName !== "" && inputs.lastName !== "" && inputs.email !== "" && inputs.passwordConfirm !== "")}*/}
+                    <Button variant={"primary"} type={"submit"} >Sign Up</Button>
 
                     <a onClick={() => {
                         setLoginScreen(!loginScreen)
